7712c69b3a73c0a3c87ed56f135faaf3da182f29
AleVor <alevor657@gmail.com>
2017-04-02 06:19:57 +0100

    First version

---
+++ config/route/base.php
@@ -0,0 +1,46 @@
+<?php
+
+
+
+/**
+ * Home
+ *
+ */
+$app->router->add("", function () use ($app) {
+    $app->view->add("take1/header", ["title" => "Home"]);
+    $app->view->add("take1/navbar");
+    $app->view->add("take1/home");
+
+    $app->response->setBody([$app->view, "render"])->send();
+});
+
+
+
+/**
+ * About
+ *
+ */
+$app->router->add("about", function () use ($app) {
+    $app->view->add("take1/header", ["title" => "Home"]);
+    $app->view->add("take1/navbar");
+    $app->view->add("take1/about");
+
+    $app->response->setBody([$app->view, "render"])->send();
+});
+
+
+
+/**
+ * Details
+ */
+$app->router->add("status", function () use ($app) {
+    $data = [
+        "Server" => php_uname(),
+        "PHP version" => phpversion(),
+        "Included files" => count(get_included_files()),
+        "Memory used" => memory_get_peak_usage(true),
+        "Execution time" => microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'],
+    ];
+
+    $app->response->sendJson($data);
+});
---
+++ src/App/App.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Alvo16\App;
+
+/**
+ * An App class to wrap the resources of the framework.
+ */
+class App
+{
+
+}
