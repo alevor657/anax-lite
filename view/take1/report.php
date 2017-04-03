718901ed751117b64fa11c490bc76572650a38fd
AleVor <alevor657@gmail.com>
2017-04-03 12:57:48 +0100

    Added report text

--- view/take1/report.php
+++ view/take1/report.php
@@ -1,3 +1,14 @@
 <h2>Kmom01</h2>
 <hr>
-<p class="report-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<h3>Hur känns det att hoppa rakt in i klasser med PHP, gick det bra?</h3>
+
+<p class="report-text">Det var lite jobbigt för mig för att jag inte har kodat i php ett tag. Det kändes som att jag använder php för första gången, så att jag har spenderat en hel dag på att klura ut hur skulle jag bygga guess spelet. Guiden php20, de båda två hjälpte mig mycket att hoppa in i php världen igen.
+Jag tycker att det var lite förmycket saker i en enda kmom. Det var ett tag sedan vi kodade i php, så det var lite stressigt denna veckan, i alla fall för mig.</p>
+
+<h3>Berätta om dina reflektioner kring ramverk, anax-lite och din me-sida.</h3>
+
+<p class="report-text">Jag har inte mycket att säga, massa saker händer där bakom som jag inte har full koll på. Men detta principen att injekta i ett objekt är nu solklar för mig. Jag tycker att det vore bättre om vi skulle bygga alla moduler till alax-lite själva eller med hjälp av övningar. Förmycket ctr – c ctrl – v i denna kmom, tycker jag. Dock så tog det ganska länge för mig att utföra alla uppgifter för att jag har glömt en del av php. Jag tycker att det skulle vara smidigare att ta varje modul som vi laddar hem med composer och förklara det i var sin artikel. På så sätt skulle det kännas lite mer som att vi bygger vårt ramverk själva och faktiskt förstår vad är det som händer bakom kulisserna.</p>
+
+<h3>Gick det bra att komma igång med MySQL, har du liknande erfarenheter sedan tidigare?</h3>
+
+<p class="report-text">Det gick väl bra, jag har jobbat igenom de första 5 uppgifter. Inget nytt eller svårt, för att vi har ju koll på SQL sedan tidigare. Jag ar använt SQL i python projektet och rena SQL satser i htmlphp. Så än så länge var det inget nytt för mig.</p>
